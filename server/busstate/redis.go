e13c254e9ea3ff1af1d4840047f60ea5cb8e87c0
hongruiqi <hongruiqi@gmail.com>
2013-04-14 19:41:04 +0800

    busstat (partly)

---
+++ server/busstate/redis.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"github.com/garyburd/redigo/redis"
+	"log"
+	"flag"
+)
+
+var (
+	redisServer string
+	redisPasswd string
+)
+
+func init() {
+	flag.StringVar(&redisServer, "redis_server", "127.0.0.1:6379", "redis server")
+	flag.StringVar(&redisPasswd, "redis_passwd", "BBTWREDIS", "redis password")
+}
+
+// 连接 redis
+func RedisConnect(redisServer string, redisPasswd string) (conn redis.Conn, err error) {
+	conn, err = redis.Dial("tcp", redisServer)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	// 认证
+	_, err = conn.Do("AUTH", redisPasswd)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	return
+}
