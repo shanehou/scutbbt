89cc837a772cf4b133980d7f0553215580cc2e99
hongruiqi <hongruiqi@gmail.com>
2013-04-14 20:31:35 +0800

    busstate (done)

--- server/busstate/busdatahandler.go
+++ server/busstate/busdatahandler.go
@@ -3,23 +3,44 @@ package main
 import (
 	. "../types"
 	"encoding/json"
+	"flag"
+	"fmt"
 	"github.com/garyburd/redigo/redis"
+	"os"
 	"time"
 )
+
+const (
+	FLY_DISTANCE = 0.05
+)
+
+var (
+	flyLogFile string
+)
+
+func init() {
+	flag.StringVar(&flyLogFile, "fly_log_path", "fly.log", "fly log path")
+}
 
 type BusDataHandler struct {
 	redisConn redis.Conn
 }
 
+// 设置最后收到GPRS数据时间
 func (h *BusDataHandler) touchBus(busName string) {
-	_, err := h.redisConn.Do("HSET", "BUS:"+busName, "LastGPRS", time.Now().Unix())
+	_, err := h.redisConn.Do("HSET", "BUS:"+busName+":INFO", "LastGPRS", time.Now().Unix())
+	if err != nil {
+		panic(err)
+	}
+	_, err = h.redisConn.Do("EXPIRE", "BUS:"+busName+":INFO", 60)
 	if err != nil {
 		panic(err)
 	}
 }
 
+// 从redis中获取校巴
 func (h *BusDataHandler) fetchBus(busName string) (coordIndex int, isNew bool) {
-	reply, err := redis.Values(h.redisConn.Do("HMGET", "BUS:"+busName, "Name", "CoordIndex"))
+	reply, err := redis.Values(h.redisConn.Do("HMGET", "BUS:"+busName+":INFO", "Name", "CoordIndex"))
 	if err != nil {
 		panic(err)
 	}
@@ -37,39 +58,67 @@ func (h *BusDataHandler) fetchBus(busName string) (coordIndex int, isNew bool) {
 	return
 }
 
+// 记录跑飞到文件
+func (h *BusDataHandler) recordFly(data *BusGPSData, index int, coordIndex int, distance float64) {
+	f, err := os.OpenFile(flyLogFile, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	fmt.Fprintln(f, data, index, coordIndex, distance)
+}
+
+// 判断校巴为跑飞
+func (h *BusDataHandler) isFly(distance float64) bool {
+	return distance > FLY_DISTANCE
+}
+
+// 删除校巴
+func (h *BusDataHandler) delBus(busName string) {
+	_, err := h.redisConn.Do("DEL", "BUS:"+busName+":INFO")
+	if err != nil {
+		panic(err)
+	}
+}
+
+// 设置校巴跑飞
+func (h *BusDataHandler) setFly(busName string) {
+	_, err := h.redisConn.Do("HMSET", "BUS:"+busName+":INFO",
+		"Fly", true,
+		"LastGPS", time.Now().Unix(),
+		"GPS_OK", true,
+	)
+	if err != nil {
+		panic(err)
+	}
+}
+
 func (h *BusDataHandler) handleGPSData(data *BusGPSData) {
 	busName := data.Name
 
-	// 从redis中获取校巴索引，方向和是否为新校巴
 	oldCoordIndex, isNew := h.fetchBus(busName)
 
 	// 获取最近的站点，设置站名，索引和距离
 	index, percent, distance, coordIndex := GetClosest(data.Latitude, data.Longitude)
-	if distance > 0.05 {
-		// TODO: Record Fly
-		_, err := h.redisConn.Do("HMSET", "BUS:"+busName,
-			"Fly", true,
-			"LastGPS", time.Now().Unix(),
-			"GPS_OK", true,
-		)
-		if err != nil {
-			panic(err)
-		}
-		h.touchBus(busName)
+
+	// 校巴在总站，校巴已停，删除校巴
+	if index == 0 {
+		h.delBus(busName)
+		return
 	}
 
-	// 校巴在总站，删除校巴
-	if index == 0 {
-		_, err := h.redisConn.Do("DEL", "BUS:"+busName)
-		if err != nil {
-			panic(err)
-		}
+	// 判断跑飞
+	if h.isFly(distance) {
+		h.recordFly(data, index, coordIndex, distance)
+		h.setFly(busName)
+		h.touchBus(busName)
 	}
 
 	var stationIndex int
 	var station string
 	var direction bool
 
+	// 站点为0时，强制设置为第一个站
 	if index == 0 {
 		stationIndex = 1
 		station = IndexToStation[1]
@@ -89,16 +138,20 @@ func (h *BusDataHandler) handleGPSData(data *BusGPSData) {
 		}
 	}
 
+	// 在车场，百分比强制归0
 	if index == 1 && percent < 0 || index == 0 {
 		percent = 0
 	}
 
+	// 反向，百分比相反
 	if direction == true {
 		percent *= -1
 	}
 
-	_, err := h.redisConn.Do("HMSET", "BUS:"+busName,
+	_, err := h.redisConn.Do("HMSET", "BUS:"+busName+":INFO",
 		"Name", busName,
+		"Latitude", data.Latitude,
+		"Longitude", data.Longitude,
 		"CoordIndex", coordIndex,
 		"StationIndex", stationIndex,
 		"Station", station,
@@ -106,6 +159,7 @@ func (h *BusDataHandler) handleGPSData(data *BusGPSData) {
 		"Direction", direction,
 		"LastGPS", time.Now().Unix(),
 		"GPS_OK", true,
+		"FLY", false,
 	)
 	if err != nil {
 		panic(err)
@@ -119,7 +173,7 @@ func (h *BusDataHandler) handleTipInfo(data *BusTipInfo) {
 	case "GPS ERROR":
 		fallthrough
 	case "GPS_WRONG":
-		_, err := h.redisConn.Do("HSET", "BUS:"+busName, "GPS_OK", false)
+		_, err := h.redisConn.Do("HSET", "BUS:"+busName+":INFO", "GPS_OK", false)
 		if err != nil {
 			panic(err)
 		}
--- server/busstate/redis.go
+++ server/busstate/redis.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"flag"
 	"github.com/garyburd/redigo/redis"
 	"log"
-	"flag"
 )
 
 var (
